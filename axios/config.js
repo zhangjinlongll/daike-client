8cdc7afcef7cc2fb531280dc00c6bcb59c4187c1
dragon <zhangjinlongll@163.com>
2019-10-27 21:30:53 +0800

    fix: ip

--- axios/config.js
+++ axios/config.js
@@ -3,14 +3,14 @@
  * @Email: frankxjkuang@gmail.com
  * @Date: 2018-04-23 09:22:51
  * @Last Modified by: dragon
- * @Last Modified time: 2019-10-13 23:07:58
+ * @Last Modified time: 2019-10-27 21:07:27
  * @Description: axios 默认的配置
  */
 import api from '../config/api'
 export default {
     method: 'post',
     // 基础url前缀
-    baseURL: 'http://106.54.126.156:8081',
+    baseURL: 'http://106.54.126.156:3000',
     // 请求头信息
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
